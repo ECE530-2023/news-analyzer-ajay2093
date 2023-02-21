68cb537c5c34e1482eabd2677dbd810027deeccd
ajay2093 <ajaykr@bu.edu>
2023-02-21 15:10:25 -0500

    Additional cases

--- test_api.py
+++ test_api.py
@@ -11,4 +11,50 @@ class TestFileUploader:
         result = uploader.upload_file(file_contents, file_name, file_location, access_control)
         
         assert result == "File uploaded successfully"
-        
+
+
+class TestTextAnalysisAPI(unittest.TestCase):
+    def setUp(self):
+        self.api = TextAnalysisAPI()
+
+    def test_analyze_text(self):
+        text = "The quick brown fox jumps over the lazy dog"
+        result = self.api.analyze_text(text)
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, dict)
+        self.assertIn("tokens", result)
+        self.assertIn("sentences", result)
+        self.assertIn("entities", result)
+
+import unittest
+
+class TestNewsFeedAPI(unittest.TestCase):
+    def setUp(self):
+        self.api = NewsFeedAPI()
+
+    def test_ingest_feed(self):
+        feed_url = "https://example.com/feed.xml"
+        result = self.api.ingest_feed(feed_url)
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, dict)
+        self.assertIn("status", result)
+        self.assertIn("message", result)
+        self.assertEqual(result["status"], "success")
+        self.assertEqual(result["message"], "Feed ingested successfully")
+
+    def test_retrieve_feed(self):
+        start_date = "2023-02-01"
+        end_date = "2023-02-28"
+        result = self.api.retrieve_feed(start_date, end_date)
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+        for item in result:
+            self.assertIsInstance(item, dict)
+            self.assertIn("title", item)
+            self.assertIn("link", item)
+            self.assertIn("description", item)
+            self.assertIn("pub_date", item)
+            self.assertIn("categories", item)
+
+
