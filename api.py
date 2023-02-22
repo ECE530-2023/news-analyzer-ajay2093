7f18506c3e45d44dcd6be941fad38c4179f5be21
ajay2093 <ajaykr@bu.edu>
2023-02-21 21:47:59 -0500

    More changes and test cases

--- api.py
+++ api.py
@@ -6,7 +6,11 @@ class FileUploader:
         """
         Upload a file to the server
         """
-        return "File uploaded Successfully"
+        extension = file_name.split()
+        if extension[-1] == "txt":
+            return "File uploaded Successfully"
+        
+        return "This file format is not supported"
     
     def retrieve_file(self, file_name: str, file_location: str, user_id: str) -> bytes:
         """
@@ -18,38 +22,63 @@ class FileUploader:
         """
         Delete a file from the server
         """
-        pass
+        for file in self.uploaded_files:
+            if file["id"] == file_id:
+                return {"message": f"File {file_id} deleted successfully."}, 200
+        return {"error": f"File {file_id} not found."}, 404
 
 class TextNLP:
     
-    def submit_text(self, text: str):
-        """
-        Submit File to server
-        """
-        pass
-
-    def analysis_type(self, type: str):
-        """
-        Submit type to Server
-        """
-        pass
+    def analyze_text(self, text: str, type:str) -> Dict[str, any]:
+        if text:
+            if type in ["sen","ner"]:
+                return {
+                    "sentiment": {"polarity": 0.7, "positive": 0.6, "negative": 0.4},
+                    "topics": ["politics", "activism", "health"],
+                    "entities": {"New Delhi": "CITY", "Narendra Modi": "PERSON"}
+                }
+        else:
+            return "Invalid input"
 
     def get_results(self, name: str):
         """
         Retrieve results
         """
-        pass
+        return {
+    "language": "en",
+    "sentences": [
+        {
+        "text": {
+            "content": "Enjoy your vacation!",
+            "beginOffset": 0
+        },
+        "sentiment": {
+            "magnitude": 0.8,
+            "score": 0.8
+        }
+        }
+    ]
+    }
 
 class NewsFeedAPI:
     def ingest_feed(self, feed_url):
         # TODO: Retrieve the feed from the given URL,
         # parse the feed data, and store the results
         # in a database for later retrieval
-        pass
+        if(not feed_url):
+            return "Invalid request"
+        else:
+            return "Feed ingested successfully."
 
     def retrieve_feed(self, start_date, end_date):
         # TODO: Retrieve all feed items from the database
         # that fall within the given date range, and return
         # them in a structured format
-        pass
+        if not source:
+            return "Empty Date"
+        else:
+            return [
+            {"title": "Article 1", "content": "This is article 1.", "source": "CNN"},
+            {"title": "Article 2", "content": "This is article 2.", "source": "BBC"}
+            ]
 
--- test_api.py
+++ test_api.py
@@ -1,21 +1,35 @@
-import api
-class TestFileUploader:
-    
+import api as api
+import unittest
+
+class TestFileUploader(unittest.TestCase):
+    def setUp(self):
+        self.api = api.FileUploader()
+
     def test_upload_file(self):
-        uploader = FileUploader()
+        uploader = api.FileUploader()
         file_contents = b"example file contents"
         file_name = "example_file.txt"
         file_location = "/example/path/"
         access_control = {"users": ["user1", "user2"], "groups": ["group1"]}
         
-        result = uploader.upload_file(file_contents, file_name, file_location, access_control)
+        result = self.api.upload_file(file_contents, file_name, file_location, access_control)
         
-        assert result == "File uploaded successfully"
-
+        self.assertEqual(result,"File uploaded successfully")
+    
+    def test_upload_file_none(self):
+        uploader = api.FileUploader()
+        file_contents = b"example file contents"
+        file_name = "example_file.api"
+        file_location = "/example/path/"
+        access_control = {"users": ["user1", "user2"], "groups": ["group1"]}
+        
+        result = self.api.upload_file(file_contents, file_name, file_location, access_control)
+        
+        self.assertEqual(result,"This file format is not supported")
 
 class TestTextAnalysisAPI(unittest.TestCase):
     def setUp(self):
-        self.api = TextAnalysisAPI()
+        self.api = api.TextNLP()
 
     def test_analyze_text(self):
         text = "The quick brown fox jumps over the lazy dog"
@@ -25,22 +39,23 @@ class TestTextAnalysisAPI(unittest.TestCase):
         self.assertIn("tokens", result)
         self.assertIn("sentences", result)
         self.assertIn("entities", result)
+    
+    def test_empty_text(self):
+        analysis = api.TextNLPAnalysis()
+        response =  analysis.analyze_text("") 
+        self.assertEqual(response, "Invalid input")
 
 import unittest
 
 class TestNewsFeedAPI(unittest.TestCase):
     def setUp(self):
-        self.api = NewsFeedAPI()
+        self.api = api.NewsFeedAPI()
 
     def test_ingest_feed(self):
         feed_url = "https://example.com/feed.xml"
         result = self.api.ingest_feed(feed_url)
         self.assertIsNotNone(result)
-        self.assertIsInstance(result, dict)
-        self.assertIn("status", result)
-        self.assertIn("message", result)
-        self.assertEqual(result["status"], "success")
-        self.assertEqual(result["message"], "Feed ingested successfully")
+        self.assertEqual(result, "Feed ingested successfully")
 
     def test_retrieve_feed(self):
         start_date = "2023-02-01"
@@ -56,5 +71,6 @@ class TestNewsFeedAPI(unittest.TestCase):
             self.assertIn("description", item)
             self.assertIn("pub_date", item)
             self.assertIn("categories", item)
+
 
 
